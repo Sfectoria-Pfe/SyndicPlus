903344c5035d1284e5edd3cf734b7bfdc48cf6f7
ali <elhabibali82@gmail.com>
2024-09-08 17:15:11 +0200

    aloulou5

--- server/models/feedback.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const feedbackSchema =  new mongoose.Schema({
-    name:String,
-    message:String,
-})
-
-const feedbackModel = mongoose.model('feedback', feedbackSchema);
-export default feedbackModel
